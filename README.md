f617f83e84515f01d32bc0b2c3f8189bfdeafc49
sai <ai.marsrovermanipal@gmail.com>
2025-08-23 19:04:52 +0530

    Initial commit with LFS for .dae files

---
+++ README.md
@@ -0,0 +1,3 @@
+# ERC-2024_AIA_2026
+
+world file and the rasperry pi camera code along with the camera tuning config file
---
+++ testSai.py
@@ -0,0 +1,36 @@
+from flask import Flask, Response
+from picamera2 import Picamera2
+import cv2
+
+app = Flask(__name__)
+
+tuning_data = Picamera2.load_tuning_file("/home/mrm/tuning.json")
+picam2 = Picamera2(tuning=tuning_data)
+
+config = picam2.create_preview_configuration(main={"format": "RGB888", "size": (1000, 1000)})
+picam2.configure(config)
+picam2.start()
+print("Camera works")
+
+def generate_frames():
+    while True:
+        frame = picam2.capture_array()
+#        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        ret, buffer = cv2.imencode('.jpg', frame)
+        if not ret:
+            continue
+        frame_bytes = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+@app.route('/')
+def index():
+    return '<h1>RPi Camera Stream</h1><img src="/video_feed">'
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, threaded=True)
